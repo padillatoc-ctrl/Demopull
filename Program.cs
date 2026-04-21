7a39dabee71f50a9e252fe9d36ef0a5c3610f8a7
padillatoc-ctrl <padillatoc@gmail.com>
2026-04-21 07:26:02 -0600

    Add Personaje class with greeting functionality

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System;
+class Personaje 
+{
+    public string nombre;
+    public int puntos;
+    public Personaje(string NombreInicial, int PuntosInicial)
+    {
+        nombre  = NombreInicial;
+        puntos=PuntosInicial;
+        Console.WriteLine("El personaje "+nombre+" ha sido creado");
+    }
+    public void Saludar()
+    {
+        Console.WriteLine(nombre+" tiene "+puntos+" puntos.");
+    }
+    
+}
+class Program
+{
+    static void Main()
+    {
+        Personaje heroe=new Personaje("Fulano", 1060);
+        heroe.Saludar();
+    }
+}
